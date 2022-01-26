74185993672b453c24547b7a2329b8bd11e165fc
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2022-01-25 21:30:53 -0300

    Desafio banco dio (Java)

---
+++ desafio-dio-banco/src/ContaPoupanca.java
@@ -0,0 +1,11 @@
+
+public abstract class ContaPoupanca extends Conta {
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupança ===");
+		super.imprimirInfosComuns();
+	
+	}
+
+}
